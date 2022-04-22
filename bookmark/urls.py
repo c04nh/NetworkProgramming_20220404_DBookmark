d57e877826aac6128ee371104fa304c3eabfae07
nhsally <nhsally@naver.com>
2022-04-22 13:32:05 +0900

    go bookmark_list in bookmark_detail

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -5,7 +5,7 @@ from bookmark.views import BookmarkListView, BookmarkCreateView, BookmarkDetailV
 app_name = 'bookmark'
 
 urlpatterns = [
-    path('list/', BookmarkListView.as_view(), name='list'),
+    path('', BookmarkListView.as_view(), name='list'),
     path('add/', BookmarkCreateView.as_view(), name='add'),
     path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail')
 ]
