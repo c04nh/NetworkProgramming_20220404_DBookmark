d4211bd5540160f1bf4f23b5d0433ce9b77f6aba
nhsally <nhsally@naver.com>
2022-06-10 14:44:57 +0900

    DBookmark/urls, accounts/urls accounts:register

--- DBookmark/urls.py
+++ DBookmark/urls.py
@@ -18,8 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from bookmark.views import BookmarkListView
+
 urlpatterns = [
-    path('', include('bookmark.urls')),
+    path('', BookmarkListView.as_view(), name='home'),
+    path('bookmark', include('bookmark.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
 
--- bookmark/urls.py
+++ bookmark/urls.py
@@ -6,9 +6,9 @@ from bookmark.views import BookmarkListView, BookmarkCreateView, BookmarkDetailV
 app_name = 'bookmark'
 
 urlpatterns = [
-    path('', BookmarkListView.as_view(), name='list'),
-    path('add/', BookmarkCreateView.as_view(), name='add'),
-    path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'),
-    path('edit/<int:pk>/', BookmarkUpdateView.as_view(), name='edit'),
-    path('delete/<int:pk>/', BookmarkDeleteView.as_view(), name='delete'),
+    path('list/', BookmarkListView.as_view(), name='list'),  # bookmark:list
+    path('add/', BookmarkCreateView.as_view(), name='add'),  # bookmark:add
+    path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'),  #bookmark:detail
+    path('edit/<int:pk>/', BookmarkUpdateView.as_view(), name='edit'),  #bookmark:edit
+    path('delete/<int:pk>/', BookmarkDeleteView.as_view(), name='delete'),  #bookmark:delete
 ]
